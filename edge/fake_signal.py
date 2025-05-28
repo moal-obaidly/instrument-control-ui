b47de3409d27f9dbad86dbb0b04ce1adde86da09
mo pc <moalobaidly@gmail.com>
2025-05-28 20:32:47 +0100

    added basic MQTT code to test sending the experiment data one by one to the broker

--- edge/fake_signal.py
+++ edge/fake_signal.py
@@ -1,9 +1,22 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import paho.mqtt.client as mqtt
+#MQTT setup
+broker_ip = "192.168.1.4" 
+port = 1883
+topic = "experiment/data"
+
+client = mqtt.Client()
+client.connect(broker_ip, port, 60)
 
 t = np.linspace(0, 1, 1000)
 signal = np.sin(2 * np.pi * 10 * t)
 
+for value in signal:
+    client.publish(topic, str(value))
+    print("Sent:", value)
+    time.sleep(0.001)  # simulate 1 kHz sample rate
+    
 plt.plot(t, signal)
 plt.title("Sine Wave")
 plt.xlabel("Time (s)")
