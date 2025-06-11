2c5b57de22ba8c3c4c56aa102fc29fc120a81153
Mo <2952593a@student.gla.ac.uk>
2025-06-11 13:25:12 +0100

    changed subscriber ip

--- ui/zeromq_attempt1_subscriber.py
+++ ui/zeromq_attempt1_subscriber.py
@@ -4,7 +4,7 @@ import time
 
 context = zmq.Context()
 socket = context.socket(zmq.SUB)
-socket.connect("tcp://localhost:5555")  # Replace <sender_ip> with actual IP or use "localhost" if same machine
+socket.connect("tcp://192.168.1.34:5555")  
 socket.setsockopt_string(zmq.SUBSCRIBE, "")
 
 while True:
