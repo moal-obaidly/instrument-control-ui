8ceed69068e03260337177b470e381adbdf36087
Mo <2952593a@student.gla.ac.uk>
2025-07-25 13:32:09 +0100

    updated the combined publisher code

--- edge/combined_1/updated_published_combined.py
+++ edge/combined_1/updated_published_combined.py
@@ -38,6 +38,22 @@ zmq_pub.bind("tcp://*:5556")
 rtt_pub = context.socket(zmq.PUB)
 rtt_pub.bind("tcp://*:5558")
 
+
+# subscriber logic to get control from ui
+sub_socket = context.socket(zmq.SUB)
+sub_socket.connect("tcp://192.168.1.66:5557")  # 36 for laptop, 82 for rpi 4, 66 for reterminal, 65 for reterminal ethernet, tailscale:100.113.46.57
+sub_socket.connect("tcp://192.168.1.36:5557")
+sub_socket.connect("tcp://192.168.1.62:5557")
+sub_socket.connect("tcp://100.85.112.65:5557")
+sub_socket.connect("tcp://100.113.46.57:5557")
+
+sub_socket.setsockopt_string(zmq.SUBSCRIBE, "experiment/control")
+sub_socket.setsockopt_string(zmq.SUBSCRIBE, "experiment/slider")
+sub_socket.setsockopt_string(zmq.SUBSCRIBE, "experiment/rateslider")
+sub_socket.setsockopt_string(zmq.SUBSCRIBE, "experiment/rate")
+sub_socket.setsockopt_string(zmq.SUBSCRIBE, "experiment/rtt/response")
+sub_socket.setsockopt_string(zmq.SUBSCRIBE, "experiment/reset")
+
 # main Functions
 def rtt():
     while running:
@@ -186,6 +202,75 @@ def on_message(client, userdata, msg):
         checksum = count = singles_sent = batches_sent = 0
         seq_num = 1
         buffered_data.clear()
+
+def ui_controls():
+    global checksum,count,singles_sent,batches_sent,seq_num,buffered_data
+    
+    while True:
+        try:
+            msg = sub_socket.recv_string()
+            parts = msg.split(" ", 1)
+            topic = parts[0]
+            payload = parts[1] if len(parts) > 1 else ""
+
+            print(f"Received on {topic}: {payload}")
+
+            if topic == "experiment/control":
+                if payload == "1":
+                    global signal_thread
+                    if signal_thread is None or not signal_thread.is_alive():
+                        signal_thread = threading.Thread(target=start_signal, daemon=True)
+                        signal_thread.start()
+                elif payload == "0":
+                    stop_signal()
+                    
+
+            elif topic == "experiment/slider":
+                try:
+                    global freq
+                    freq = float(payload)
+                    print("Updated frequency to:", freq)
+                except ValueError:
+                    print("Invalid frequency value:", payload)
+
+            elif topic in ["experiment/rateslider", "experiment/rate"]:
+                try:
+                    global rate
+                    rate = float(payload)
+                    print("Updated sampling rate to:", rate)
+                except ValueError:
+                    print("Invalid rate value:", payload)
+
+            elif topic == "experiment/rtt/response":
+                try:
+                    
+                    orig_time = float(payload)
+                    rtt_ms = (time.time() - orig_time) * 1000
+                    rtt_socket.send_string(f"experiment/rtt/display {rtt_ms}")
+                    print(f"RTT: {rtt_ms:.2f} ms")
+                except ValueError:
+                    print("Invalid RTT response value:", payload)
+            elif topic == "experiment/reset":
+                try:
+                    if payload == "1" :
+                        checksum = 0
+                        count = 0
+                        singles_sent = 0
+                        batches_sent = 0
+                        seq_num = 1
+                        buffered_data.clear()
+                        
+                    
+                except ValueError:
+                    print("Invalid time value:", payload)
+
+        except zmq.ZMQError as e:
+            print("ZMQ error:", e)
+            break
+
+# Start control listener in background
+ui_thread = threading.Thread(target=ui_controls, daemon=True)
+ui_thread.start()
 
 # MQTT init
 client.on_connect = on_connect
